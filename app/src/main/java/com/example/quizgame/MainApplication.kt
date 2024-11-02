dfd36f6c4554b8e78c0de50f3334a964cd654fc1
harijoshi <hariojoshi@gmail.com>
2024-11-02 08:36:48 +0545

    fix: add viewModelModule in MainApplication to start Koin with the module

--- app/src/main/java/com/example/quizgame/MainApplication.kt
+++ app/src/main/java/com/example/quizgame/MainApplication.kt
@@ -2,6 +2,8 @@ package com.example.quizgame
 
 import android.app.Application
 import com.example.quizgame.di.appModule
+import com.example.quizgame.di.networkModule
+import com.example.quizgame.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
@@ -14,7 +16,7 @@ class MainApplication : Application() {
         startKoin {
             androidLogger()
             androidContext(this@MainApplication)
-            modules(appModule)
+            modules(appModule, networkModule, viewModelModule)
 
         }
     }
