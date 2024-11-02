cdae58916a389fd8085c354f6754b9cec00d1aa4
harijoshi <hariojoshi@gmail.com>
2024-11-02 09:01:47 +0545

    feat: add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,5 +95,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+    //splash screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 
 }
--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.quizgame.presentation.navigation.NavigationScreen
 import com.example.quizgame.ui.theme.QuizGameTheme
 
@@ -18,6 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             QuizGameTheme {
                 NavigationScreen()
