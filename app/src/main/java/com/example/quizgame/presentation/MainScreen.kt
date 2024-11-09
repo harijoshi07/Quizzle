397fe4f21a4aa9a08e863f4cf42caf965b534658
harijoshi <hariojoshi@gmail.com>
2024-11-09 14:39:15 +0545

    feat: add navigation to pass data between screens

--- app/src/main/java/com/example/quizgame/presentation/MainScreen.kt
+++ app/src/main/java/com/example/quizgame/presentation/MainScreen.kt
@@ -14,14 +14,19 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.quizgame.R
+import com.example.quizgame.data.SettingsQuiz
 import com.example.quizgame.presentation.navigation.Screen
 import com.example.quizgame.ui.component.BottomBar
 import com.example.quizgame.ui.component.FloatingButton
 import com.example.quizgame.ui.component.TopBar
 import com.example.quizgame.ui.screen.category.CategoryScreen
+import com.example.quizgame.ui.screen.category.CategorySelectScreen
 import com.example.quizgame.ui.screen.home.HomeScreen
 import com.example.quizgame.ui.screen.profile.ProfileScreen
-import com.example.quizgame.ui.screen.shared.ListScreen
+import com.example.quizgame.ui.screen.quiz.QuizModeScreen
+import com.example.quizgame.ui.screen.quiz.QuizResultScreen
+import com.example.quizgame.ui.screen.quiz.QuizSelectScreen
+import com.example.quizgame.ui.screen.quiz.QuizStartScreen
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
@@ -44,19 +49,75 @@ fun MainScreen(modifier: Modifier = Modifier) {
         NavHost(navController = navController, startDestination = Screen.Home.route) {
 
             composable(route = Screen.Home.route) {
-                HomeScreen(innerPadding = innerPadding)
+                HomeScreen(
+                    navigateToQuiz = { navController.navigate(route = Screen.SelectQuiz.route + "/$it") },
+                    navigateToCategory = { navController.navigate(route = Screen.Category.route) },
+                    innerPadding = innerPadding
+                )
             }
 
             composable(route = Screen.Category.route) {
-                CategoryScreen(modifier = Modifier.padding(innerPadding))
+                CategoryScreen(
+                    navigateToQuiz = { navController.navigate(route = Screen.SelectQuiz.route + "/$it") },
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+
+            composable(Screen.SelectQuiz.route + "/{quizCategory}") {
+                val quizCategory = it.arguments?.getString("quizCategory") ?: ""
+                CategorySelectScreen(quizCategory = quizCategory, navigateQuiz = { quiz ->
+                    navController.navigate(Screen.QuizStart.route + "/$quizCategory/$quiz")
+                })
             }
 
             composable(route = Screen.Profile.route) {
                 ProfileScreen(modifier = Modifier.padding(innerPadding))
             }
 
-            composable(route = Screen.List.route) {
-                ListScreen(modifier = Modifier.padding(innerPadding))
+            //quiz Screen
+            composable(Screen.QuizStart.route + "/{quizCategory}/{quiz}") {
+                val quizCategory = it.arguments?.getString("quizCategory") ?: ""
+                val quiz = it.arguments?.getString("quiz") ?: ""
+                val settingsQuiz = it.savedStateHandle.get<SettingsQuiz>("settingsQuiz")
+
+                QuizStartScreen(quizCategory = quizCategory,
+                    quiz = quiz,
+                    settingsQuiz = settingsQuiz ?: SettingsQuiz(),
+                    navigateToSelect = {
+                        navController.navigate(Screen.QuizSelect.route)
+                    },
+                    navigateToGames = {
+                        navController.currentBackStackEntry?.savedStateHandle?.set(
+                            "settingsQuizStart", it
+                        )
+                        navController.navigate(Screen.QuizMode.route + "/$quiz/$quizCategory")
+                    })
+            }
+
+            composable(Screen.QuizSelect.route) {
+                QuizSelectScreen(navigateToStart = {
+                    navController.previousBackStackEntry?.savedStateHandle?.set(
+                        "settingsQuiz", it
+                    )
+                    navController.popBackStack()
+                })
+            }
+
+            composable(Screen.QuizMode.route + "/{quiz}/{category}") {
+                val settingsQuizStart =
+                    navController.previousBackStackEntry?.savedStateHandle?.get<SettingsQuiz>("settingsQuizStart")
+                        ?: SettingsQuiz()
+                val quizName = it.arguments?.getString("quiz") ?: ""
+                val category = it.arguments?.getString("category") ?: ""
+                QuizModeScreen(
+                    settingsQuiz = settingsQuizStart,
+                    quizName = quizName,
+                    category = category,
+                    navigateToResult = { correctAnswer, size ->
+                        navController.navigate(
+                            Screen.QuizResult.route + "/null/$correctAnswer/$size/$quizName/$category"
+                        )
+                    })
             }
 
         }
