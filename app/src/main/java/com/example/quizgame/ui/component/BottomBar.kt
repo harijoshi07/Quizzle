3a02665c56606614e8f1423c9111ea12daede7fa
harijoshi <hariojoshi@gmail.com>
2024-11-11 08:54:52 +0545

    refactor: update colors of bottom bar

--- app/src/main/java/com/example/quizgame/ui/component/BottomBar.kt
+++ app/src/main/java/com/example/quizgame/ui/component/BottomBar.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.quizgame.R
 import com.example.quizgame.data.itemNavigation
 import com.example.quizgame.presentation.navigation.Screen
 
@@ -33,6 +37,11 @@ fun BottomBar(
             NavigationBarItem(
                 selected = isSelected,
                 onClick = { navController.navigate(item.title) },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = colorResource(id = R.color.primary_purple),
+                    unselectedIconColor = colorResource(id = R.color.secondary_blue),
+                    indicatorColor = colorResource(id = R.color.white)
+                ),
                 icon = {
                     if (item.title != "WhiteSpace") {
                         Icon(
